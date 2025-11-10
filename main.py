aeea9b76e2c04ebe3878bf76fd1c2f51d0df932b
UD <yudibarak770@gmail.com>
2025-11-10 16:55:37 +0200

    json flow update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from classes.book import Book
 from classes.library import Library
 from classes.user import User
-
+import manager
 
 book1 = Book("potter", "j.k.rowling")
 book2 = Book("chaim", "asher")
@@ -10,12 +10,11 @@ user1 = User("shay", 208795658,[])
 user2 = User("yishay", 208795333,[])
 
 
-library = Library()
-library.add_book(book1)
-library.add_user(user1)
+library1 = Library()
+library1.add_book(book1)
+library1.add_user(user1)
 
-print(library.borrow_book(208795658, book1.ISBN))
-print(user1) 
-# for book in user1.borrowed_books:
-#     print(book)
-
+print(library1.borrow_book(208795658, book1.ISBN))
+print(user1)
+handle = manager.FileHandling()
+handle.book_to_json(library1)
