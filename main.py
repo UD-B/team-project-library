e8956afe6de4d4ce92da42a3d8966ab219a29653
ShayShN <shayshn1234@gmail.com>
2025-11-10 17:59:03 +0200

    json

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-from re import search
 from classes.book import Book
 from classes.library import Library
 from classes.user import User
+import manager
+
 
 
 arr_books = [Book("potter", "j.k.rowling"), Book("chaim", "asher"), Book("ring", "guy guy")]
 arr_users = [User("shay", 208795658,[]), User("yishay", 208795333,[]), User("zeava", 2087953,[])]
 
 
-library = Library()
+library1 = Library()
 
 for book in arr_books:
-    library.add_book(book)
+    library1.add_book(book)
 
 for user in arr_users:
-    library.add_user(user)
+    library1.add_user(user)
 
-print(library.borrow_book(208795658, 587658767))
+print(library1.borrow_book(208795658, 587658767))
 # print([str(i) for i in arr_books]) 
 
-print(library.list_available_books())
+print(library1.list_available_books())
 
-search_book = library.search_book("potter")
+search_book = library1.search_book("potter")
 print(search_book)
+
+manager1 = manager.FileHandling()
+manager1.json_book(library1) 
